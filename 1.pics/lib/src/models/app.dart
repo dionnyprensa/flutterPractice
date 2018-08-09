9cba176fd7b04ef6d923797825bb3c5f1bb063cf
Dionny Prensa <d.prensa1@gmail.com>
2018-08-09 16:52:19 -0400

    [ADD] Image Model is added

--- 1.pics/lib/src/models/app.dart
+++ 1.pics/lib/src/models/app.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import "package:http/http.dart" show get;
+import "models/ImageModel.dart";
 
 class App extends StatefulWidget{
   createState(){
@@ -6,8 +8,14 @@ class App extends StatefulWidget{
   }
 }
 
+
 class AppState extends State<App>{
   int counter = 0;
+
+  void fecthImage(){
+    
+  }
+
   Widget build(context){
     return MaterialApp(
     home: Scaffold(
@@ -18,11 +26,7 @@ class AppState extends State<App>{
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         tooltip: "Add an image",
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
+        onPressed: fecthImage,
       ),
     ),
   );
