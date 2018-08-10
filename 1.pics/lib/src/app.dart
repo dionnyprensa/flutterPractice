de78af36709eaa5ade2739e0284052483c87b214
Dionny Prensa <d.prensa1@gmail.com>
2018-08-10 17:19:29 -0400

    [ADD] Widget model is added

--- 1.pics/lib/src/app.dart
+++ 1.pics/lib/src/app.dart
@@ -2,6 +2,7 @@ import "dart:convert";
 import "package:flutter/material.dart";
 import "package:http/http.dart" show get;
 import "models/ImageModel.dart";
+import "widgets/imagesList.dart";
 
 class App extends StatefulWidget{
   createState(){
@@ -11,11 +12,16 @@ class App extends StatefulWidget{
 
 class AppState extends State<App>{
   int counter = 0;
+  List<ImageModel> images = [];
 
   void fecthImage() async {
     this.counter++;
     var response = await get('http://jsonplaceholder.typicode.com/photos/$counter');
     var imageModel = ImageModel.fromJSON(json.decode(response.body));
+    
+    setState(() {
+      images.add(imageModel);
+    });
   }
 
   Widget build(context){
@@ -24,7 +30,7 @@ class AppState extends State<App>{
       appBar: AppBar(
         title: Text("Let's See Images!"),
       ),
-      body: Text("$counter"),
+      body: ImageList(images),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         tooltip: "Add an image",
---
+++ 1.pics/lib/src/widgets/imagesList.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import '../models/ImageModel.dart';
+
+class ImageList extends StatelessWidget{
+  final List<ImageModel> images;
+
+  ImageList(this.images);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: images.length,
+      itemBuilder: (context, int index){
+
+      },
+    );
+  } 
+}
+
+// flutter upgrade packages
